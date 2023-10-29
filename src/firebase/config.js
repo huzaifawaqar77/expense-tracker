8be1ae056fc22831e7a32aa225fba86801691faa
huzaifa <huzaifawaqar77@gmail.com>
2023-10-29 14:03:54 +0500

    add the firebaseConfig inside the src/firebase/config.js file of your own

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,6 @@ import "firebase/firestore";
 import "firebase/auth";
 
 // firebae Config
-const firebaseConfig = {
-  apiKey: "AIzaSyAGOpzJ1c30-ih3c54wW-Lyf3PyBk0_7nQ",
-  authDomain: "recipe-directory-nextjs.firebaseapp.com",
-  projectId: "recipe-directory-nextjs",
-  storageBucket: "recipe-directory-nextjs.appspot.com",
-  messagingSenderId: "94181069324",
-  appId: "1:94181069324:web:655079f85cf6a998361429",
-};
 
 // Initialize Firebase
 if (!firebase.apps.length) {
